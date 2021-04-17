8635fad212c519b875c8b1eeb44a344ddf184fdc
Fernando Izquierdo <227fer@gmail.com>
2021-04-17 17:08:36 +0000

    Loggin de inicio/cierre de sesión mediante las signals

--- mi_sitio_web/settings.py
+++ mi_sitio_web/settings.py
@@ -134,3 +134,47 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOG_FILE = os.path.join(BASE_DIR, 'Server.log')
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+				
+	'formatters': {
+		'verbose': {
+			'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+									'datefmt': "%d/%b/%Y %H:%M:%S"
+							},
+		'simple': {
+			'format': '%(levelname)s [%(name)s:%(lineno)s] %(message)s'
+							},
+						},
+				
+	'handlers': {
+		'file': {
+			'level': 'INFO',
+			'class': 'logging.FileHandler',
+			'filename': os.path.join(BASE_DIR, LOG_FILE),
+			'formatter': 'verbose',
+			'mode': 'w'
+			},
+		'console': {
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler',
+			'formatter': 'simple'
+		}
+	},
+				
+	'loggers': {
+		'django': {
+			'handlers': ['file'],
+			'propagate': True,
+			'level': 'ERROR',
+		},
+		'': {
+				'handlers': ['file', 'console'],
+				'level': 'DEBUG',
+			}
+		}
+	}	
--- senderos/apps.py
+++ senderos/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class SenderosConfig(AppConfig):
     name = 'senderos'
+
+    def ready(self):
+        import senderos.signals
