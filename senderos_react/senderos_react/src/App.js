443d43fe57fe89ef5bf9960e4079b45c7cc0c115
Fernando Izquierdo <227fer@gmail.com>
2021-06-06 11:53:45 +0000

    Index con las cards

--- senderos_react/senderos_react/src/App.js
+++ senderos_react/senderos_react/src/App.js
@@ -14,20 +14,37 @@ class App extends React.Component{
     fetch('http://localhost:8000/api/excursiones')
     .then(res=>res.json())
     .then(datos=>{
-      console.log(datos)
+      this.setState({excursiones: datos})
     })
   }
 
   render(){
     return (
-      <Container fluid>
-        <Navbar bg="light">
-          <Navbar.Brand href="#home">
-            <img alt="" src={logo} width="50" height="30" className="d-inline-block align-top"/>
-            <span style={{marginLeft:'2vw'}}>Senderos Granada- React</span>
-          </Navbar.Brand>
-        </Navbar>
-      </Container>
+      <div>
+        <Container fluid> 
+          <Navbar bg="light">
+            <Navbar.Brand href="#home">
+              <img alt="" src={logo} width="50" height="30" className="d-inline-block align-top"/>
+              <span style={{marginLeft:'2vw'}}>Senderos Granada- React</span>
+            </Navbar.Brand>
+          </Navbar>
+        </Container>
+        <div className="container" style={{marginTop: '15px'}}>
+          <h1>Bienvenido a Senderos</h1>
+          <div className="d-flex flex-wrap mt-5 justify-content-around">
+            {this.state.excursiones.map((e) => (
+              <div className="card me-3 mb-5" style={{width: '18rem'}}>
+                <img className="card-img-top" src={"http://localhost:8000/static/images/" + e.file}/>
+                <div className="card-body">
+                  <h5>{e.nombre}</h5>
+                  <p className="card-text">{e.descripcion}</p>
+                  <a href="#" className="btn btn-primary">Más información</a>
+                </div>
+              </div>
+            ))}
+          </div>  
+        </div>      
+      </div>     
     );
   }
 }
